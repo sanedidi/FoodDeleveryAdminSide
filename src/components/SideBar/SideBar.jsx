00fe5af12c9055d431b628428eec66a1e820b4aa
Shaxzod <sanedidi98@gmail.com>
2024-05-08 15:10:53 +0500

    sidebar edited

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import s from "./SideBar.module.scss";
 import { Box } from "@chakra-ui/react";
-import { LogoIcon } from "../SvgComponents/SvgComponents";
+import { LogoIcon, SettingsIcon } from "../SvgComponents/SvgComponents";
 import useSideBarProps from "./useSideBarProps";
 import { Link } from "react-router-dom";
-import {
-  BarIcon,
-  ClientsIcon,
-  GroupsIcon,
-  LocationIcon,
-  RestaurantIcon,
-  ShopIcon,
-} from "../SvgComponents/SvgComponents";
 const SideBar = () => {
-  const { sideBarLinks } = useSideBarProps();
+  const { sideBarLinks, settings } = useSideBarProps();
   return (
     <Box className={s.sidebar}>
-      <Box className={s.sidebar__top}>
-        <Box className={s.sidebar__logo}>
-          <LogoIcon width={"36px"} height={"36px"} />
-        </Box>
-      </Box>
-      <Box className={s.sidebar__bottom}>
-        {sideBarLinks.map((el, index) => {
-          return (
-            <Box className={s.sidebar__links} key={index}>
-              <Link to={"/"}>{el.icon}</Link>
+      <Box className={s.sidebar__wrapper}>
+        <Box className={s.sidebar__main_top}>
+          <Box className={s.sidebar__top}>
+            <Box className={s.sidebar__logo}>
+              <LogoIcon width={"36"} height={"36"} />
             </Box>
-          );
-        })}
+          </Box>
+          <Box className={s.sidebar__bottom}>
+            {sideBarLinks.map((el, index) => {
+              return (
+                <Box className={s.sidebar__links} key={index}>
+                  <Link to={el.path}>{el.icon}</Link>
+                </Box>
+              );
+            })}
+          </Box>
+        </Box>
+        <Box className={s.sidebar__main_bottom}>
+          {settings.map((el, index) => {
+            return (
+              <Box
+                className={el.id === 1 ? s.sidebar__set : s.sidebar__acc}
+                key={index}
+              >
+                {el.icon}
+              </Box>
+            );
+          })}
+        </Box>
       </Box>
     </Box>
   );
--- src/components/SideBar/useSideBarProps.jsx
+++ src/components/SideBar/useSideBarProps.jsx
@@ -6,21 +6,36 @@ import {
   GroupsIcon,
   LocationIcon,
   RestaurantIcon,
+  SettingsIcon,
   ShopIcon,
+  UserIcon,
 } from "../SvgComponents/SvgComponents";
 
 const useSideBarProps = () => {
   const sideBarLinks = [
-    { id: 1, icon: <BarIcon />, path: "" },
-    { id: 2, icon: <ShopIcon />, path: "" },
-    { id: 3, icon: <ClientsIcon />, path: "" },
-    { id: 4, icon: <GroupsIcon />, path: "" },
-    { id: 5, icon: <LocationIcon />, path: "" },
-    { id: 6, icon: <RestaurantIcon />, path: "" },
-    { id: 7, icon: <DateIcon />, path: "" },
+    {
+      id: 1,
+      icon: <BarIcon width={26} height={26} />,
+      path: "/admin/dashboard",
+    },
+    { id: 2, icon: <ShopIcon width={26} height={26} />, path: "" },
+    {
+      id: 3,
+      icon: <ClientsIcon width={26} height={26} />,
+      path: "/admin/clients",
+    },
+    { id: 4, icon: <GroupsIcon width={26} height={26} />, path: "" },
+    { id: 5, icon: <LocationIcon width={26} height={26} />, path: "" },
+    { id: 6, icon: <RestaurantIcon width={26} height={26} />, path: "" },
+    { id: 7, icon: <DateIcon width={26} height={26} />, path: "" },
+  ];
+  const settings = [
+    { id: 1, icon: <SettingsIcon width={26} height={26} />, path: "" },
+    { id: 2, icon: <UserIcon width={26} height={26} />, path: "" },
   ];
   return {
     sideBarLinks,
+    settings,
   };
 };
 
