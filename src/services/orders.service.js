08bcb04641bfff05a19324ff5b275ed8629f420f
sanedidi98@gmail.com <sanedidi98@gmail.com>
2024-05-27 16:56:12 +0500

    update orders.service.js

--- src/services/orders.service.js
+++ src/services/orders.service.js
@@ -1,20 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import request from "./httpRequest";
+import { useMutation } from "react-query";
+import queryClient from "./queryClient";
 
 const OrdersService = {
   getOrders: (params) =>
-    axios
-      .get(
-        `https://food-delivery-api-n6as.onrender.com/v1/orders
-      `,
-        { params }
-      )
-      .then((res) => res?.data.product),
+    request
+      .get("https://food-delivery-api-n6as.onrender.com/v1/orders", {
+        params,
+      })
+      .then((res) => res?.data),
+  deleteOrders: (orderId) =>
+    request(
+      `https://food-delivery-api-n6as.onrender.com/v1/order/${orderId}`
+    ).then((res) => res?.data),
 };
 
 export const useGetOrdersService = (params) => {
   return useQuery({
     queryKey: ["products", params],
     queryFn: () => OrdersService.getOrders(params),
+  });
+};
+
+export const useDeleteOrder = () => {
+  return useMutation({
+    mutationFn: (orderId) => OrdersService.deleteOrders(orderId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
   });
 };
