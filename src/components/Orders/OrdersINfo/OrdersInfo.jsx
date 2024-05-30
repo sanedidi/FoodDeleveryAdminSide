ec0bb3db10153c56722b6a96d530956e9721b3ea
sanedidi98@gmail.com <sanedidi98@gmail.com>
2024-05-30 19:54:00 +0500

    update OrdersInfo.jsx

--- src/components/Orders/OrdersINfo/OrdersInfo.jsx
+++ src/components/Orders/OrdersINfo/OrdersInfo.jsx
@@ -29,7 +29,7 @@ const OrdersInfo = () => {
                 to={"/admin/orders"}
               >
                 <CloseIcon color={"red"} />
-                Закрыть
+                Закрыть 
               </Link>
             }
           />
