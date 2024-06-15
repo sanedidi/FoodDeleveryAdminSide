584ac93c71244766de118a25aaacfd67c792d9a4
sanedidi <sanedidi98@gmail.com>
2024-06-15 14:52:13 +0500

    update Dashboard.jsx, data.js and useDashboardProps.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,37 +1,84 @@
-import { data } from "./data";
-import s from "./Dashboard.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import clsx from "clsx";
-import {
-  Box,
-  CustomBtn,
-  CustomInput,
-  FilterIcon,
-  Header,
-  UnderHeader,
-} from "public/imports";
+import { Box, Header } from "public/imports";
 import useDashboardProps from "./useDashboardProps";
-import { Calendar } from "primereact/calendar";
+import s from "./Dashboard.module.scss";
+import { format, subDays, subMonths, startOfDay } from "date-fns";
 
 function calc(part, whole) {
   if (whole === 0) {
     return 0;
   }
-  return (part / whole) * 100;
+  return (part / whole) * 1000;
+}
+
+function formatDateToMonthYear(dateString) {
+  const date = new Date(dateString);
+  const options = { year: "numeric", month: "long" };
+  return date.toLocaleDateString("uz-Uz", options);
 }
 
 export default function Dashboard() {
-  const [key, setKey] = useState(Object.keys(data)?.[0]);
-  const array = Array.from({ length: data[key].limit }, (_, i) => i);
   const [selectedFromDate, setSelectedFromDate] = useState(null);
   const [selectedToDate, setSelectedToDate] = useState(null);
-  // const [searchParams, setSearchParams] = useState({ search: "" });
 
-  const { stats, stat } = useDashboardProps();
+  const {
+    stats,
+    stat,
+    orders,
+    isLoading,
+    currentPage,
+    setCurrentPage,
+    pageSize,
+    getStats,
+  } = useDashboardProps();
+
   const handleInputClear = () => {
     setSelectedFromDate(null);
     setSelectedToDate(null);
+    handleFilter("view_all");
   };
+
+  const handleFilter = (filterType) => {
+    let fromDate = null;
+    let toDate = null;
+    const today = startOfDay(new Date());
+
+    switch (filterType) {
+      case "12_months":
+        fromDate = subMonths(today, 12);
+        break;
+      case "6_months":
+        fromDate = subMonths(today, 6);
+        break;
+      case "30_days":
+        fromDate = subDays(today, 30);
+        break;
+      case "7_days":
+        fromDate = subDays(today, 7);
+        break;
+      case "view_all":
+        break;
+      default:
+        break;
+    }
+
+    if (filterType !== "view_all") {
+      toDate = format(today, "yyyy-MM-dd");
+    }
+
+    const formattedFromDate = fromDate ? format(fromDate, "yyyy-MM-dd") : null;
+    const formattedToDate = toDate;
+
+    setSelectedFromDate(formattedFromDate);
+    setSelectedToDate(formattedToDate);
+
+    getStats(formattedFromDate, formattedToDate);
+  };
+
+  useEffect(() => {
+    handleFilter("view_all");
+  }, []);
 
   return (
     <>
@@ -41,8 +88,8 @@ export default function Dashboard() {
           <Box className={s.dash__select}>
             <input
               type="datetime-local"
-              onChange={(e) => setSelectedFromDate(e.value)}
-              value={selectedFromDate}
+              onChange={(e) => setSelectedFromDate(e.target.value)}
+              value={selectedFromDate || ""}
               placeholder="Выберите дату"
               className={s.dash__underHeader_input}
             />
@@ -52,8 +99,8 @@ export default function Dashboard() {
           <Box className={s.dash__select}>
             <input
               type="datetime-local"
-              onChange={(e) => setSelectedToDate(e.value)}
-              value={selectedToDate}
+              onChange={(e) => setSelectedToDate(e.target.value)}
+              value={selectedToDate || ""}
               placeholder="Выберите дату"
               className={s.dash__underHeader_input}
             />
@@ -64,72 +111,93 @@ export default function Dashboard() {
         }
       />
       <Box className={s.dash__top}>
-        {stat.map((el) => {
-          return (
-            <Box className={s.dash__item} key={el.id}>
-              <Box
-                className={
-                  el.id === 1
-                    ? s.dash__id1
-                    : el.id === 2
-                    ? s.dash__id2
-                    : el.id === 3
-                    ? s.dash__id3
-                    : el.id === 4
-                    ? s.dash__id4
-                    : s.default_classname
-                }
-              >
-                {el.icon}
-              </Box>
-
-              <Box className={s.dash__info}>
-                <h2 className={s.dash__info_title}>{el.status}</h2>
-                <p>{el.quant}</p>
-              </Box>
+        {stat.map((el) => (
+          <Box className={s.dash__item} key={el.id}>
+            <Box
+              className={
+                el.id === 1
+                  ? s.dash__id1
+                  : el.id === 2
+                  ? s.dash__id2
+                  : el.id === 3
+                  ? s.dash__id3
+                  : el.id === 4
+                  ? s.dash__id4
+                  : s.default_classname
+              }
+            >
+              {el.icon}
             </Box>
-          );
-        })}
+
+            <Box className={s.dash__info}>
+              <h2 className={s.dash__info_title}>{el.status}</h2>
+              <p>{el.quant}</p>
+            </Box>
+          </Box>
+        ))}
       </Box>
 
       <div style={{ display: "flex", padding: "10px" }}>
         <div className={s.patientStatistics}>
           <div className={s.header}>
-            <div className={s.title}>Отчет о продаж</div>
+            <div className={s.title}>Отчет о продажах</div>
 
             <div className={s.btns}>
-              {Object.keys(data).map((el) => (
-                <div
-                  className={clsx(s.btn, key == el && s.activeKey)}
-                  onClick={() => setKey(el)}
-                  key={el}
-                >
-                  {el}
-                </div>
-              ))}
+              <div
+                className={clsx(s.btn)}
+                onClick={() => handleFilter("12_months")}
+              >
+                12 Месяцев
+              </div>
+              <div
+                className={clsx(s.btn)}
+                onClick={() => handleFilter("6_months")}
+              >
+                6 Месяцев
+              </div>
+              <div
+                className={clsx(s.btn)}
+                onClick={() => handleFilter("30_days")}
+              >
+                30 Дней
+              </div>
+              <div
+                className={clsx(s.btn)}
+                onClick={() => handleFilter("7_days")}
+              >
+                7 Дней
+              </div>
+              <div
+                className={clsx(s.btn)}
+                onClick={() => handleFilter("view_all")}
+              >
+                Посмотреть все
+              </div>
             </div>
           </div>
 
           <div className={s.table}>
             <div className={s.fromToLimit}>
-              {array.map((el) => (
-                <span key={el}>{el * 10}</span>
+              {Array.from({ length: pageSize }, (_, i) => i * 10).map((el) => (
+                <span key={el}>{el}</span>
               ))}
             </div>
 
-            <div className={s.items} key={key}>
-              {data?.[key]?.data.map((el, i) => (
+            <div className={s.items}>
+              {orders.map((el, i) => (
                 <div
                   className={s.item}
                   style={{
-                    height: `${calc(el?.numberOfPatients, data[key].limit)}%`,
+                    height: `${calc(el?.total_price, pageSize * 10)}%`,
                   }}
-                  key={el.label || i}
+                  key={el.id || i}
                 >
                   <div className={s.line}>
                     <div></div>
                   </div>
-                  <span className={s.subKey}>{el.label}</span>
+                  <span className={s.subKey}>
+                    {formatDateToMonthYear(el.created_at)}
+                  </span>
                 </div>
               ))}
             </div>
--- src/components/Dashboard/useDashboardProps.jsx
+++ src/components/Dashboard/useDashboardProps.jsx
@@ -1,45 +1,36 @@
+import { useEffect, useState } from "react";
+import request from "services/httpRequest";
 import {
   DashIcon1,
   DashIcon2,
   DashIcon3,
   DashIcon4,
 } from "components/SvgComponents/SvgComponents";
-import { format } from "date-fns";
-import { useEffect, useState } from "react";
-import request from "services/httpRequest";
 
 const useDashboardProps = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1); // pagination
   const [pageSize, setPageSize] = useState(10); // pagination
-  const [totalPages, setTotalPages] = useState(10); // pagination
+  const [totalPages, setTotalPages] = useState(100); // pagination
   const [orders, setOrders] = useState([]); // Store orders data
   const [stats, setStats] = useState([]);
   const [fil, setfil] = useState([]);
-  const getStats = async (
-    page = 1,
-    limit,
-    search = "",
-    from_date = "",
-    to_date = ""
-  ) => {
+
+  const getStats = async (from_date = "", to_date = "") => {
     setIsLoading(true);
 
     try {
       const response = await request.get("/dashboard", {
         params: {
-          page,
-          limit,
-          search,
           from_date,
           to_date,
         },
       });
+
       const fetchedStats = response.data?.Data?.orders;
       setStats(fetchedStats);
       if (fetchedStats) {
         setOrders(fetchedStats);
-        setTotalPages(Math.ceil(response.data?.Data?.count / limit));
       }
     } catch (error) {
       console.error(error);
@@ -51,12 +42,14 @@ const useDashboardProps = () => {
   useEffect(() => {
     getStats(currentPage, pageSize);
   }, [currentPage, pageSize]);
+
   const stat = [
     { id: 1, status: "Завершенные", quant: "123,000", icon: <DashIcon1 /> },
     { id: 2, status: "Общяя сумма", quant: "60,458,000", icon: <DashIcon2 /> },
     { id: 3, status: "Отмененные", quant: "23,000", icon: <DashIcon3 /> },
     { id: 4, status: "Все заказы", quant: "2,000,000", icon: <DashIcon4 /> },
   ];
+
   return {
     getStats,
     orders,
